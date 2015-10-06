ef1c3ecbd88973360e0f59a2ab6af6ab81794ebf
John Haviland <haviland.10@osu.edu>
2015-10-05 23:33:50 -0400

    Expense Activity and Expense Summary Activity made. Trying to get Parse to work, but currently running into errors.

---
+++ app/src/main/java/cse5321/roommateapp/ExpenseActivity.java
@@ -0,0 +1,21 @@
+package cse5321.roommateapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense);
+    }
+
+
+//    public void SummaryButtonClick(View v) {
+//        Intent intent = new Intent(this, //Name of Muy summry Activity);
+//        startActivity(intent);
+//    }
+}
