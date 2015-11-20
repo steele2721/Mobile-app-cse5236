eca3398d28fb83b708651be505e4e23bd20d09d2
Ryan Faulhaber <faulhaber.14@osu.edu>
2015-11-19 21:28:10 -0500

    ExpenseList now looks and behaves like grocery list, minus checkboxes (I didn't know if we wanted checkboxes in the expense list or if there was something else we were going to do).

--- app/src/main/java/cse5321/roommateapp/ExpenseListAdapter.java
+++ app/src/main/java/cse5321/roommateapp/ExpenseListAdapter.java
@@ -1,12 +1,15 @@
 package cse5321.roommateapp;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 /**
@@ -33,14 +36,43 @@ public class ExpenseListAdapter extends ArrayAdapter<Expense> {
             v = vi.inflate(R.layout.expense_item_layout, null);
         }
 
-        Expense item = getItem(position);
+        final Expense item = getItem(position);
 
-        // TODO: add more fields!! Format to look nice!
         if (item != null) {
             TextView expenseNameView = (TextView) v.findViewById(R.id.expense_item_name);
+            TextView expenseAmountView = (TextView) v.findViewById(R.id.expense_item_amount);
 
             if (expenseNameView != null) {
                 expenseNameView.setText(item.getName());
+
+                expenseNameView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder summaryBuilder = new AlertDialog.Builder(v.getContext());
+
+                        summaryBuilder.setMessage("Type: "
+                                + item.getType() + "\nPaid by: "
+                                + item.getPaidBy() + "\nAmount: "
+                                + NumberFormat.getCurrencyInstance().format(item.getPrice())
+                                + "\nDue date: " + item.getDueDate());
+
+                        summaryBuilder.setTitle(item.getName());
+
+                        summaryBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // doesn't need to do anything!
+                            }
+                        });
+
+                        AlertDialog summaryDialog = summaryBuilder.create();
+                        summaryDialog.show();
+                    }
+                });
+            }
+
+            if (expenseAmountView != null) {
+                expenseAmountView.setText(NumberFormat.getCurrencyInstance().format(item.getPrice()));
             }
         }
 
